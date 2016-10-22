f925b90d5e7ece489ee32f0937f6c4cb3c763abb
Robert Buckley <cardinalsmurf@gmail.com>
2016-10-21 17:57:04 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,7 @@
 var myTitle = document.querySelector('p');
 myTitle.textContent = "Hello, Teenage America.";
+
+var myButton = document.querySelector('button');
+myButton.onclick = function() {
+  document.getElementById("demo").innerHTML = "You clicked me!";
+}
